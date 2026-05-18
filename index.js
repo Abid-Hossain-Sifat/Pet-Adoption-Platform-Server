6def4a0692db7c4cf6e0b289310d093e4f899d14
Abid-Hossain-Sifat <itzabid006@gmail.com>
2026-05-19 03:13:22 +0600

    get data from mongoDB

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require ('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 
@@ -10,6 +11,44 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT;
+const uri = process.env.MongoDB_URI;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+const run = async () => {
+    try {
+        await client.connect();
+
+        const Data = client.db('Pets')
+        const collection = Data.collection('all_pets')
+
+        app.get ('/pets', async (req, res) => {
+            const cursor = collection.find ()
+            const result = await cursor.toArray()
+
+            res.send (result)
+        })
+
+
+        await client.db('admin').command ({ ping: 1 })
+        console.log ('ping deploy successfully')
+    }
+    catch (error) {
+        console.log (error)
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
 
 
 
