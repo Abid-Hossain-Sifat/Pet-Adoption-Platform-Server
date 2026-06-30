de2ba1c1a20a06ea52439955aaf43609c499e638
Abid-Hossain-Sifat <itzabid006@gmail.com>
2026-06-30 19:32:24 +0600

    fix login issue

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 
+const dns = require('dns');
+dns.setServers(['1.1.1.1', '8.8.8.8']);
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
